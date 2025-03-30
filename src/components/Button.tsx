decb30ed4d3ea12b94811a0bd0885820df89bd5d
Manish Chahar <manishchahar148@gmail.com>
2025-03-31 00:09:33 +0530

    updates typing

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ className, children, ...props }) => {
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  children,
+  ...props
+}) => {
   return (
     <button
       className={`px-4 py-2 cursor-pointer bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition ${className}`}
